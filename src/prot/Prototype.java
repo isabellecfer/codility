7a4902943f49386821a10fb2d34ef0a45f4c47f1
Isabelle Ferreira <isabellecfer@gmail.com>
2016-04-05 02:59:56 -0300

    BinaryGap solved.

---
+++ src/prot/Prototype.java
@@ -0,0 +1,20 @@
+package prot;
+
+/**
+ * 
+ * @author Isabelle Ferreira
+ *
+ */
+public class Prototype {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println(solution(0));
+	}
+
+	public static int solution(int N){
+		
+		return 0;
+	}
+}
